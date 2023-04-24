19a971d784a82a80b254cc747f7058ef11fcdafe
su4ovden <denskykin381@gmail.com>
2023-04-24 20:00:56 +0300

    Fixed checking the bit for the correct operation of the PLL module

--- prototype_5kW/firmware/clock.c
+++ prototype_5kW/firmware/clock.c
@@ -14,10 +14,10 @@ void clock_init(void)
 
 	// Configure clock map
 	RCC->CFGR &= ~RCC_CFGR_RTCPRE;
-	RCC->CFGR |= RCC_CFGR_RTCPRE & ((uint8_t)25 << RCC_CFGR_RTCPRE_Pos); // HSE prescaler for RTC (1 MHz)
+	RCC->CFGR |= RCC_CFGR_RTCPRE & (25 << RCC_CFGR_RTCPRE_Pos); // HSE prescaler for RTC (1 MHz)
 
 	RCC->CFGR &= ~RCC_CFGR_HPRE; // AHB no prescaling
-	RCC->CFGR &= ~RCC_CFGR_PPRE2; // APB high-speed no prescalin
+	RCC->CFGR &= ~RCC_CFGR_PPRE2; // APB high-speed no prescaling
 
 	RCC->CFGR &= ~RCC_CFGR_PPRE1;
 	RCC->CFGR |= RCC_CFGR_PPRE1_DIV2; // APB low-speed with prescaler 2
@@ -33,13 +33,13 @@ void clock_init(void)
 	RCC->PLLCFGR &= ~RCC_PLLCFGR_PLLN;
 
 	// Set value for PLL coefficients
-	RCC->PLLCFGR |= RCC_PLLCFGR_PLLQ & ((uint8_t)8 << RCC_PLLCFGR_PLLQ_Pos);  // Q coifficient
-	RCC->PLLCFGR |= RCC_PLLCFGR_PLLM & ((uint8_t)25 << RCC_PLLCFGR_PLLM_Pos); // M coifficient
-	RCC->PLLCFGR |= RCC_PLLCFGR_PLLN & ((uint16_t)328 << RCC_PLLCFGR_PLLN_Pos); // N coifficient
+	RCC->PLLCFGR |= RCC_PLLCFGR_PLLQ & (8 << RCC_PLLCFGR_PLLQ_Pos);  // Q coifficient
+	RCC->PLLCFGR |= RCC_PLLCFGR_PLLM & (25 << RCC_PLLCFGR_PLLM_Pos); // M coifficient
+	RCC->PLLCFGR |= RCC_PLLCFGR_PLLN & (328 << RCC_PLLCFGR_PLLN_Pos); // N coifficient
 	RCC->PLLCFGR |= RCC_PLLCFGR_PLLP_0; // P coifficient division by 4
 
 	RCC->CR |= RCC_CR_PLLON; // Enable PLL
-	while(RCC->CR & RCC_CR_PLLRDY_Msk); // Wait untill PLL is ready
+	while(!(RCC->CR & RCC_CR_PLLRDY_Msk)); // Wait untill PLL is ready
 
 	RCC->CFGR &= ~RCC_CFGR_SW;
 	RCC->CFGR |= RCC_CFGR_SW_PLL; // Set PLL as system clock
